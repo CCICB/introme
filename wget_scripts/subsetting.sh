c03e332a44a2fc5375c0909458fc3f9a415ec1b1
pj-sullivan <33969604+pj-sullivan@users.noreply.github.com>
2022-05-03 17:46:20 -0400

    Create subsetting.sh

---
+++ wget_scripts/subsetting.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define inputs for subsetting portion of introme
+while getopts "b:p:v:" opt; do
+    case $opt in
+        b) input_BED="$OPTARG";; # Input BED file (i.e. regions of interest)
+        p) prefix="$OPTARG";; # Output file prefix
+        v) input_VCF="$OPTARG";; # Input VCF file
+    esac
+done
+shift $(( $OPTIND - 1 )) # Remove parsed options and args from $@ list
+
+
+echo $(gzip -d -c $input_VCF | grep -v '^#' | wc -l) 'variants in input VCF'
+
+# Correctly formats the input VCF file for Introme
+bcftools sort $input_VCF | uniq | bgzip > output/$prefix.sorted.vcf.gz # Ensures the file is sorted correctly prior to subsetting
+bcftools norm -m-both output/$prefix.sorted.vcf.gz | bgzip > output/$prefix.sorted.norm.vcf.gz # Removes multiallelics
+
+echo $(gzip -d -c $input_VCF | grep -v '^#' | wc -l) 'variants prior to subsetting'
+
+echo $(date +%x_%r) 'Beginning subsetting to genomic regions of interest'
+bedtools intersect -header -u -a output/$prefix.sorted.norm.vcf.gz -b $input_BED | bgzip > output/$prefix.subset.vcf.gz # -u for unique record in VCF
+
+tabix -p vcf $out_dir/working_files/$prefix.subset.vcf.gz
+
+echo $(gzip -d -c output/$prefix.subset.vcf.gz | grep -v '^#' | wc -l) 'variants after subsetting'
+echo $(date +%x_%r) 'Subsetting complete'
