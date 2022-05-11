f3e8254e62924d1d57a993fd9b1cfc2cd0c53ae6
pj-sullivan <33969604+pj-sullivan@users.noreply.github.com>
2022-05-11 17:06:03 -0400

    Update filter.sh

--- wget_scripts/filter.sh
+++ wget_scripts/filter.sh
@@ -13,9 +13,9 @@ shift $(( $OPTIND - 1 )) # Remove parsed options and args from $@ list
 echo $(date +%x_%r) 'Beginning filtering by annotation values'
 
 if [ $allele_freq < 1 ]; then
-    bcftools filter --threads $(getconf _NPROCESSORS_ONLN) -i"(MGRB_AF<=$allele_freq || MGRB_AF='.') && (gnomAD_PM_AF<=$allele_freq || gnomAD_PM_AF='.')" $prefix.subset.highquality.annotated.vcf.gz | bgzip > $prefix.subset.highquality.annotated.filtered.vcf.gz
+    bcftools filter --threads $(getconf _NPROCESSORS_ONLN) -i"(MGRB_AF<=$allele_freq || MGRB_AF='.') && (gnomAD_PM_AF<=$allele_freq || gnomAD_PM_AF='.')" $input_vcf | bgzip > $prefix.subset.highquality.annotated.filtered.vcf.gz
 else
-    cp $prefix.subset.highquality.annotated.vcf.gz $prefix.subset.highquality.annotated.filtered.vcf.gz
+    cp $input_VCF $prefix.subset.highquality.annotated.filtered.vcf.gz
 fi
 tabix -p vcf $prefix.subset.highquality.annotated.filtered.vcf.gz
 
