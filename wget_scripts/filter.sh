7f9fb833aad1fee3c90f057f3bd8f3833fad64ca
pj-sullivan <33969604+pj-sullivan@users.noreply.github.com>
2022-05-11 13:34:32 -0400

    Update filter.sh

--- wget_scripts/filter.sh
+++ wget_scripts/filter.sh
@@ -12,7 +12,7 @@ shift $(( $OPTIND - 1 )) # Remove parsed options and args from $@ list
 
 echo $(date +%x_%r) 'Beginning filtering by annotation values'
 
-if [ $pop_filter < 1 ]; then
+if [ $allele_freq < 1 ]; then
     bcftools filter --threads $(getconf _NPROCESSORS_ONLN) -i"(MGRB_AF<=$allele_freq || MGRB_AF='.') && (gnomAD_PM_AF<=$allele_freq || gnomAD_PM_AF='.')" $prefix.subset.highquality.annotated.vcf.gz | bgzip > $prefix.subset.highquality.annotated.filtered.vcf.gz
 else
     cp $prefix.subset.highquality.annotated.vcf.gz $prefix.subset.highquality.annotated.filtered.vcf.gz
@@ -22,3 +22,12 @@ tabix -p vcf $prefix.subset.highquality.annotated.filtered.vcf.gz
 
 echo $(gzip -d -c $prefix.subset.highquality.annotated.filtered.vcf.gz | grep -v '^#' | wc -l) 'variants after filtering'
 echo $(date +%x_%r) 'Frequency filtering complete'
+
+
+## Prepare files for next step 
+gunzip -k $prefix.subset.highquality.annotated.filtered.vcf.gz # MMSplice and SpliceAI needs unzipped input files
+
+# Remove annotations which interfere with score extraction
+bcftools view -h $prefix.subset.highquality.annotated.filtered.vcf.gz > $prefix.subset.highquality.annotated.filtered.rmanno.vcf
+grep -v "^#" $prefix.subset.highquality.annotated.filtered.vcf | awk '{$8="."; print }' OFS='\t' >> $prefix.subset.highquality.annotated.filtered.rmanno.vcf
+bgzip $prefix.subset.highquality.annotated.filtered.rmanno.vcf
