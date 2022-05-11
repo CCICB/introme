0d4df8f467d2684a259ea0bef39e74c1fb41c4f1
pj-sullivan <33969604+pj-sullivan@users.noreply.github.com>
2022-05-11 09:28:43 -0400

    Update subsetting.sh

--- wget_scripts/subsetting.sh
+++ wget_scripts/subsetting.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Hard filter cutoffs for each annotation, filtering thresholds will depend on your application, we urge you to carefully consider these
+min_QUAL='>=200' # The QUAL VCF field
+min_DP='>=20' # The sample with the highest depth must have a equal or larger depth than this value
+min_AD='>=5' # The sample with the highest number of alternate reads must have a equal or larger number than this value
+
 # Define inputs for subsetting portion of introme
-while getopts "b:p:v:" opt; do
+while getopts "b:p:q:v:" opt; do
     case $opt in
         b) input_BED="$OPTARG";; # Input BED file (i.e. regions of interest)
         p) prefix="$OPTARG";; # Output file prefix
+        q) no_qual_filter="$OPTARG";; # trigger no quality filter command
         v) input_VCF="$OPTARG";; # Input VCF file
     esac
 done
@@ -26,3 +32,17 @@ tabix -p vcf $prefix.subset.vcf.gz
 
 echo $(gzip -d -c $prefix.subset.vcf.gz | grep -v '^#' | wc -l) 'variants after subsetting'
 echo $(date +%x_%r) 'Subsetting complete'
+
+
+echo $(date +%x_%r) 'Beginning quality filtering'
+
+if [ $no_qual_filter == 0 ]; then
+    bcftools filter --threads $(getconf _NPROCESSORS_ONLN) -i"(FILTER='PASS' || FILTER='.') && (QUAL$min_QUAL || QUAL='.') && MAX(FORMAT/DP[*])$min_DP && MAX(FORMAT/AD[*:1])$min_AD" $out_dir/working_files/$prefix.subset.vcf.gz | bgzip > $out_dir/working_files/$prefix.subset.highquality.vcf.gz
+else
+    bcftools filter --threads $(getconf _NPROCESSORS_ONLN) -i"(FILTER='PASS' || FILTER='.') && (QUAL$min_QUAL || QUAL='.')" $out_dir/working_files/$prefix.subset.vcf.gz | bgzip > $out_dir/working_files/$prefix.subset.highquality.vcf.gz
+fi
+
+tabix -p vcf $prefix.subset.highquality.vcf.gz
+
+echo $(gzip -d -c $prefix.subset.highquality.vcf.gz | grep -v '^#' | wc -l) 'variants after filtering'
+echo $(date +%x_%r) 'Quality filtering complete'
