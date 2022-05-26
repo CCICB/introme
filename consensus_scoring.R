59dd496d5a69ef7383fabc475fdf5b5bc31a2d10
pj-sullivan <33969604+pj-sullivan@users.noreply.github.com>
2022-05-26 10:23:06 -0400

    Update consensus_scoring.R

--- consensus_scoring.R
+++ consensus_scoring.R
@@ -2,38 +2,40 @@
 
 ### Script to generate consensus scores for Introme
 
-require("ROCR")
-require("caret")
-#install.packages("ROCR", repos = "http://cran.us.r-project.org", quiet = TRUE)
-#suppressPackageStartupMessages(library(ROCR))
+if(!require("ROCR")) {
+  install.packages("ROCR", repos = "http://cran.us.r-project.org", quiet = TRUE)
+  suppressPackageStartupMessages(library(ROCR))
+}
+
+if(!require("caret")) {
+  install.packages("caret", quiet = TRUE)
+  suppressPackageStartupMessages(library(caret))
+}
 
 # Read in running parameters
 args = commandArgs(trailingOnly=TRUE)
-
 Introme_file <- read.csv(args[2], header=T, sep="\t", na.strings = '.')
+Introme_model <- readRDS("models/C50_210826.rds")
 
-if (args[1] == "full") {
-  Introme_model <- readRDS("models/C50_200812.rds")
-} else if (args[1] == "fast") {
-  Introme_model <- readRDS("models/C50_Fast.rds")
-  
-  # Assign all entries which were not scored in the SpliceAI Precomputed file with a 0
-  Introme_file$SpliceAI_Acceptor_Gain[is.na(Introme_file$SpliceAI_Acceptor_Gain)] <- 0
-  Introme_file$SpliceAI_Acceptor_Loss[is.na(Introme_file$SpliceAI_Acceptor_Loss)] <- 0
-  Introme_file$SpliceAI_Donor_Gain[is.na(Introme_file$SpliceAI_Donor_Gain)] <- 0
-  Introme_file$SpliceAI_Donor_Loss[is.na(Introme_file$SpliceAI_Donor_Loss)] <- 0
-}
-
+# Ensure all columns are in the right format (for when there are missing values)
 Introme_file$SPIDEX_dPSI_Zscore <- as.numeric(Introme_file$SPIDEX_dPSI_Zscore)
 Introme_file$SPIDEX_dPSI_Max_Tissue <- as.numeric(Introme_file$SPIDEX_dPSI_Max_Tissue)
 Introme_file$dbscSNV_AdaBoost_Score <- as.numeric(Introme_file$dbscSNV_AdaBoost_Score)
 Introme_file$dbscSNV_RandomForest_Score <- as.numeric(Introme_file$dbscSNV_RandomForest_Score)
-Introme_file$AG_Created <- as.numeric(Introme_file$AG_Created)
-Introme_file$GT_Created <- as.numeric(Introme_file$GT_Created)
-Introme_file$Branchpointer_Branchpoint_Prob <- as.numeric(Introme_file$Branchpointer_Branchpoint_Prob)
+Introme_file$AG_Created <- as.factor(Introme_file$AG_Created)
+Introme_file$GT_Created <- as.factor(Introme_file$GT_Created)
+Introme_file$AG_Lost <- as.factor(Introme_file$AG_Lost)
+Introme_file$GT_Lost <- as.factor(Introme_file$GT_Lost)
+Introme_file$Branchpointer_Prob <- as.numeric(Introme_file$Branchpointer_Prob)
+Introme_file$Branchpointer_max_Prob <- as.numeric(Introme_file$Branchpointer_max_Prob)
 Introme_file$Branchpointer_U2_Binding_Energy <- as.numeric(Introme_file$Branchpointer_U2_Binding_Energy)
+Introme_file$Branchpointer_options <- as.numeric(Introme_file$Branchpointer_options)
+Introme_file$Branchpointer_max_U2_Binding_Energy <- as.numeric(Introme_file$Branchpointer_max_U2_Binding_Energy)
+Introme_file$Intron_Type[is.na(Introme_file$Intron_Type)] <- "U2"
+Introme_file$Intron_Type <- as.factor(Introme_file$Intron_Type)
+Introme_file$Gene_Regions <- as.factor(Introme_file$Gene_Regions)
 
-
+# Calculate Introme scores
 Predict_scores <- predict(Introme_model, newdata = Introme_file, type = "prob", na.action = na.pass)
 Introme_file$Introme <- Predict_scores$SAV
 
