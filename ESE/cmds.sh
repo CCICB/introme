100468f6a1f62c8223e4c1a22fdb629c0eecfd62
Donren Leung <donren.leung@gmail.com>
2023-08-25 13:35:44 +1000

    Progress on laptop 24 Aug

---
+++ ESE/cmds.sh
@@ -0,0 +1,4 @@
+
+python3 ESE_ESS_scoring.py AG_check/files/SpliceVarDB.hg38.strand.vcf ESE/working_files/SpliceVarDB.ESE.big.vcf  AG_check/files/hg38_.fa
+
+python3 ESE/scoring.py AG_check/files/SpliceVarDB.hg38.strand.vcf ESE/working_files/SpliceVarDB.ESE.big.new.vcf  AG_check/files/hg38_.fa
--- ESE/faidx.py
+++ ESE/faidx.py
@@ -2,7 +2,6 @@ import pysam
 
 def faidx_context(ref_genome: pysam.FastaFile, chrom: str, pos: int,
                   ref_allele: str, context_len: int, prepend_chr=False):
-    #TODO: Assert vcf ref == faidx ref 
     if prepend_chr and not chrom.startswith("chr"):
         chrom = f"chr{chrom}"
     elif not prepend_chr and chrom.startswith("chr"):
@@ -16,11 +15,11 @@ def faidx_context(ref_genome: pysam.FastaFile, chrom: str, pos: int,
 
     before = sequence[:context_len]
     try:
-        assert(ref_allele == sequence[context_len:context_len + ref_len])
+        assert(ref_allele == sequence[context_len:context_len + ref_len].upper())
     except AssertionError:
-        print(f"reference allele {ref_allele} did not match reference genome "
-              f"{sequence[context_len:context_len + ref_len]} at {chrom}:{pos}")
-        exit(1)
+        print(f"reference allele ({ref_allele}) did not match the provided reference genome "
+              f"({sequence[context_len:context_len + ref_len]}) at {chrom}:{pos}")
+        # exit(1)
     after = sequence[context_len + ref_len:]
 
     return (before, ref_allele, after)
@@ -28,7 +27,15 @@ def faidx_context(ref_genome: pysam.FastaFile, chrom: str, pos: int,
 def ref_and_alt(ref_genome: pysam.FastaFile, chrom: str, pos: int, ref_allele: str,
                 alt_allele: str, context_len: int, prepend_chr=False):
     before, ref_allele, after = faidx_context(ref_genome, chrom, pos, ref_allele, context_len, prepend_chr)
-    return (f"{before}{ref_allele}{after}", f"{before}{alt_allele}{after}")
+    # return (f"{before}{ref_allele}{after}", f"{before}{alt_allele}{after}")
+    return before, ref_allele, after, alt_allele
+
+def make_window(before: str, ref: str, after: str, alt: str, window_len: int):
+    window_len -= 1
+    before = before[-window_len:]
+    after = after[:window_len]
+    # return f"{before}{ref}{after}", f"{before}{alt}{after}"
+    return before, ref, after, alt
 
 if __name__ == "__main__":
     import sys
@@ -42,7 +49,8 @@ if __name__ == "__main__":
             print(f"Assertion that {a}, {[len(x) for x in a]} == {expected} failed")
 
     def test_ref_and_alt(chrom: str, pos: int, ref_allele: str, alt_allele: str, expected: tuple):
-        a = ref_and_alt(ref_genome, chrom, pos, ref_allele, alt_allele, CONTEXT_LENGTH, prepend_chr=True)
+        before, ref_allele, after, alt_allele = ref_and_alt(ref_genome, chrom, pos, ref_allele, alt_allele, CONTEXT_LENGTH, prepend_chr=True)
+        a = f"{before}{ref_allele}{after}", f"{before}{alt_allele}{after}"
         try:
             assert(a == expected)
         except AssertionError:
@@ -72,10 +80,11 @@ if __name__ == "__main__":
     test_ref_and_alt("1", 77297580, "GT", "A",   ('GTGGAAAGGGTACTCAGAGT', 'GTGGAAAGGAACTCAGAGT'))
 
     res0 = ref_and_alt(ref_genome, "1", 77297580, "G", "T", CONTEXT_LENGTH, prepend_chr=True)
-    res0 = ref_and_alt(ref_genome, "1", 77297579, "GG", "TA", CONTEXT_LENGTH, prepend_chr=True)
+    res1 = ref_and_alt(ref_genome, "1", 77297579, "GG", "TA", CONTEXT_LENGTH, prepend_chr=True)
     print(res0)
 
-    seq1, seq2 = res0
+    seq1, seq2 = "".join(res0[0:3]), "".join(res0[0] + res0[3] + res0[2])
+    print(seq1, seq2)
     # Test for win8
     window = 8
     for i in range(window):
@@ -86,7 +95,12 @@ if __name__ == "__main__":
     print(seq1[x:x + (2 * window - 1)])
     print(seq2[x:x + (2 * window - 1)])    
 
-    # print("=====")
+    print("=====")
+
+    print(make_window(*res0, 5))
+    print(make_window(*res1, 5))
+    print(make_window(*res0, 8))
+    print(make_window(*res1, 8))
 
     # window = 7
     # for i in range(window):
