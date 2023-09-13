7bc24baac5f869f75eaca3464751eac6558f5cc3
Donren Leung <donren.leung@gmail.com>
2023-09-13 13:26:19 +1000

    Added reverse strand sequence tests

--- ESE/variants.py
+++ ESE/variants.py
@@ -1,6 +1,8 @@
 import pysam
 from dataclasses import dataclass
+from Bio.Seq import reverse_complement
 
+# Variants with ref/alt allele and context window before and after
 @dataclass(frozen=True)
 class VariantContext():
     before: str
@@ -11,6 +13,7 @@ class VariantContext():
     context_length: int
 
     # TODO: find a way to not have to remake strings for context window
+    # TODO: assert context_length is long enough for window_length
     def ref_sequence(self, window_length: int=None) -> str:
         if window_length is not None:
             window_length -= 1
@@ -23,6 +26,12 @@ class VariantContext():
             return f"{self.before[-window_length:]}{self.alt_allele}{self.after[:window_length]}"
         return f"{self.before}{self.alt_allele}{self.after}"
     
+    def ref_sequence_rev(self, window_length: int=None) -> str:
+        return reverse_complement(self.ref_sequence(window_length))
+
+    def alt_sequence_rev(self, window_length: int=None) -> str:
+        return reverse_complement(self.alt_sequence(window_length))
+
     def debug(self) -> tuple:
         tup = (self.before, self.ref_allele, self.after, self.alt_allele)
         return tup, [len(elem) for elem in tup]
@@ -30,6 +39,7 @@ class VariantContext():
     def __repr__(self) -> str:
         return str(self.debug())
 
+# Variants extracted from vcf file
 @dataclass(frozen=True)
 class Variant():
     chrom: str
@@ -47,10 +57,10 @@ class Variant():
         try:
             assert(self.ref_allele == sequence[context_len:context_len + ref_len])
         except AssertionError:
-            # print(f"Variant reference allele ({self.ref_allele}) did not match the provided reference genome "
-            #     f"({sequence[context_len:context_len + ref_len]}) at {self.chrom}:{self.position}")
+            print(f"Variant reference allele ({self.ref_allele}) did not match the provided reference genome "
+                f"({sequence[context_len:context_len + ref_len]}) at {self.chrom}:{self.position}")
             # exit(1)
-            pass
+            # pass
         after = sequence[context_len + ref_len:]
 
         return VariantContext(before, self.ref_allele, self.alt_allele, after, context_len)
